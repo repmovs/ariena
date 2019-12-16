7a6a4f3255c83e2223a399672cd9a4dad5f24340
repmovs <repmovs@gmail.com>
2019-12-16 21:46:01 +0200

    cleaned project, sass and eslint packages remained

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
-
-import HomePage from "./pages/homepage/homepage.component";
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
